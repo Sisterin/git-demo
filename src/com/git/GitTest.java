2199a10901a211f8f65569a3794f67a7c389e8f6
Waw伢ya <1329853507@qq.com>
2022-03-31 20:02:19 +0800

    master  commit 4

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -3,5 +3,8 @@ package com.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("学习git1");
+        System.out.println("学习git2");
+        System.out.println("学习git3");
+        System.out.println("学习git4");
     }
 }
