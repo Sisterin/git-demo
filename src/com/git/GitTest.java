bb26f1d16a5f91e09109f2f5533242a51dbc7bae
Sisterin <1329853507@qq.com>
2022-03-31 20:31:49 +0800

    pull test

--- src/com/git/GitTest.java
+++ src/com/git/GitTest.java
@@ -8,6 +8,6 @@ public class GitTest {
         System.out.println("master test");
         System.out.println("学习git4  test");
         System.out.println("push  test");
-
+        System.out.println("pull  test");
     }
 }
